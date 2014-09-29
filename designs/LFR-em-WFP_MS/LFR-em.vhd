572d4251a27d716a80c71893e795790da728ac11
Jean-christophe Pellion <jean-christophe.pellion@easii-ic.com>
2014-09-29 13:53:13 +0200

    Update Version Number ...

--- designs/LFR-em-WFP_MS/LFR-em.vhd
+++ designs/LFR-em-WFP_MS/LFR-em.vhd
@@ -357,7 +357,7 @@ BEGIN  -- beh
       pirq_ms                => 6,
       pirq_wfp               => 14,
       hindex                 => 2,
-      top_lfr_version        => X"010118")  -- aa.bb.cc version
+      top_lfr_version        => X"01011A")  -- aa.bb.cc version
                                             -- AA : BOARD NUMBER
                                             -- 0 => MINI_LFR
                                             -- 1 => EM
