848c09304a874290071540534a3ac7e445e8ee38
Jean-christophe Pellion <jean-christophe.pellion@easii-ic.com>
2015-01-15 16:46:19 +0100

    (MINI-LFR) WFP_MS_0-1-39 => Correction of the time of waveform picker's buffer

--- designs/MINI-LFR_WFP_MS/MINI_LFR_top.vhd
+++ designs/MINI-LFR_WFP_MS/MINI_LFR_top.vhd
@@ -505,7 +505,7 @@ BEGIN  -- beh
       pirq_ms                => 6,
       pirq_wfp               => 14,
       hindex                 => 2,
-      top_lfr_version        => X"000126")  -- aa.bb.cc version
+      top_lfr_version        => X"000127")  -- aa.bb.cc version
     PORT MAP (
       clk             => clk_25,
       rstn            => LFR_rstn,
