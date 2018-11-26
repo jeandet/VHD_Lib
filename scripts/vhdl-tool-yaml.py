003243b9c7d67457a391da5a43e1593fd4bcb022
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-11-26 20:03:43 +0100

    [vhdl-tool]Fixed some bug in generator and added ieee lib support
    Generator didn't handle multiple entries per line in txt files.
    Generator now accept a path to ieee libraries.
    
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- scripts/vhdl-tool-yaml.py
+++ scripts/vhdl-tool-yaml.py
@@ -2,6 +2,17 @@
 import argparse
 import os
 import yaml
+from collections import Iterable
+
+
+def flatten(items):
+    """Yield items from any nested iterable; see Reference."""
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            for sub_x in flatten(x):
+                yield sub_x
+        else:
+            yield x
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--libs-file", help="Add libs.txt file", required=True)
@@ -9,17 +20,13 @@ parser.add_argument("--dir-skip", help="Dir skip", required=False)
 parser.add_argument("--lib-skip", help="Lib skip", required=False)
 parser.add_argument("--file-skip", help="File skip", required=False)
 parser.add_argument("--work", help="Files to add to work library", required=False)
+parser.add_argument("--ieee", help="Path to ieee library", required=False)
 
 args = parser.parse_args()
 dir_skip = (args.dir_skip or "").split(" ")
 lib_skip = (args.lib_skip or "").split(" ")
 file_skip = (args.file_skip or "").split(" ")
 work = (args.work or "").split(" ")
-
-work_lib = {
-            "name":"work",
-            "paths":[]
-            }
 
 output_file = {
     "Libraries":[],
@@ -51,14 +58,21 @@ output_file = {
     }
 current_lib = None
 
+def create_libentry(lib_name, paths):
+    return {
+        "name":lib_name,
+        "paths":paths
+    }
 
 def apply_for_each(path, function, base_path=''):
     if os.path.exists(path):
         with open(path,'r') as f:
             lines = f.readlines()
-            for l in lines:
-                l = l.split('#')[0]
-                entry = (base_path+'/'+l).strip()
+            entries = [l.split(' ') for l in lines]
+            entries = flatten(entries)
+            for e in entries:
+                e = e.split('#')[0]
+                entry = (base_path+'/'+e).strip()
                 if os.path.exists(entry) and os.path.abspath(entry)!=os.path.abspath(base_path):
                     function(entry)
 
@@ -87,8 +101,14 @@ def parse_lib(lib_path):
 def main():
     relpath = os.path.dirname(args.libs_file)
     apply_for_each(args.libs_file, parse_lib, relpath)
-    work_lib["paths"] = work
-    output_file["Libraries"].append(work_lib)
+    parse_lib(relpath+'/grlib')
+    output_file["Libraries"].append(create_libentry("work", work))
+    if args.ieee:
+        files = [os.path.join(args.ieee, f) for f in os.listdir(args.ieee) 
+                                    if (os.path.isfile(os.path.join(args.ieee, f)) and f[-4:] == '.vhd')
+                                    ]
+        output_file["Libraries"].append(create_libentry("ieee", files))
+
     yaml.dump(output_file, open('vhdltool-config.yaml','w'), default_flow_style=False)
 
 
